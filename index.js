568a524dc894a3774e2a76faa83cbfee5c35e025
Shivwant Kumar <shivwant.kumar@neutrinos.co>
2022-09-05 12:22:12 +0530

    modified pyramid pattern

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var i,j,row=5;
-str=''
-str3=' '
-str1='* '
-for(i=0;i<row;i++)
-{
-    str=''
-    for(j=row-i;j>1;j--)
-    {
-        str=str+str3;
+let i, j, row = 5;
+const str3 = ' '
+const str1 = '* '
+
+for (i = 0; i < row; i++) {
+    let str = ''
+
+    for (j = row - i; j > 1; j--) {
+        str = str + str3;
     }
-    for (j=0;j<=i;j++)
-    {
-        str=str+str1
+    for (j = 0; j <= i; j++) {
+        str = str + str1
     }
     console.log(str)
 }
+
 console.log("done")
